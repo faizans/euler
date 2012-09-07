4eb2130a7cda8de4a9ccc8157358a8d14e21cde5
Faizan Mohammad <faizan.shaharyar@gmail.com>
2012-09-07 12:51:31 +0200

    added basic python implementation

---
+++ python/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+__init__.py.py
+
+Created by PyCharm on 5/5/12 12:34 AM.
+Copyright (c) coresystems ag. All rights reserved.
+"""
+__author__ = 'faizan.shaharyar@gmail.com'
+
